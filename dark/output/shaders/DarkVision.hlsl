abe4e67db4bb94c46cfd3d2004dc9676dab6c98a
phroggster <phroggster@users.noreply.github.com>
2023-01-26 20:43:04 -0600

    Revert "Some cleanups, likely not functionality change"
    This reverts commit 0164af2ff05952161477e64de09eb24adbc17932.

--- dark/output/shaders/DarkVision.hlsl
+++ dark/output/shaders/DarkVision.hlsl
@@ -1,11 +1,7 @@
 // phrogg eyes dark.hlsl
-// 
-// phroggie's version of the commonly-seen "dark mode" AV.
-// 
 // SPDX-License-Identifier: MIT
 // Github:      https://github.com/phroggster/ns2_phrogg_eyes
-// Workshop:    https://steamcommunity.com/sharedfiles/filedetails/?id=2822011098
-// Workshop ID: 2822011098
+// Workshop ID:	2822011098
 
 #include <renderer/RenderSetup.hlsl>
 
@@ -53,97 +49,79 @@ VS_OUTPUT SFXBasicVS(VS_INPUT input)
 	return output;
 }
 
-// Sample from a texture, enforcing a clamped addressing mode.
 float4 clamp_tex2D(sampler2D tex, float2 coord)
 {
-	// TODO: remove this and make the CDT fix the sampler to use D3DTADDRESS_MIRROR instead of D3DTADDRESS_CLAMP on depthTexture.
-	const float2 zero = float2(0.0000001, 0.0000001);
-	const float2 one  = float2(0.9999999, 0.9999999);
+	const float zero = 0.000001;
+	const float one  = 0.999999;
 
-	float2 pos = clamp(coord, zero, one);
-	return tex2D(tex, pos);
+	return tex2D(tex, clamp(coord, float2(zero, zero), float2(one, one)));
 }
 
-// Compute the relative luminance (Y) of a color value in linear space using the 2.2 power curve.
-// Parameter:  RGB (float3) normalized color.
-// Result:     normalized float describing how bright the pixel is. 0 = pitch black, 1 = 100% white
-float relative_luminance(float3 color)
+float relative_luminance(float4 color)
 {
-	// https://en.wikipedia.org/wiki/Relative_luminance
-	const float Rsens = 0.2126;
-	const float Gsens = 0.7152;
-	const float Bsens = 0.0722;
-	return clamp((color.r * Rsens) + (color.g * Gsens) + (color.b * Bsens), 0, 1);
+	return clamp((color.r * 0.2126) + (color.g * 0.7152) + (color.b * 0.0722), 0, 1);
 }
 
 float4 SFXDarkVisionPS(PS_INPUT input) : COLOR0
 {
-	const float edgColorStrength = 0.875;
 	const float entColorStrength = 0.625; // 0: vanilla plus edging; 1: excessive saturation.
 	const float staticBrightness = 0.025; // 0: total black; 1: grayscale default luminance.
-
-	const float4 colorEdging = float4(0.875, 0.875, 0.0,   1.0); // #dfdf00 / (223, 223,   0) medium yellow-green
-	const float4 colorMarine = float4(0.75,  0.0,   0.625, 1.0); // #BF009F / (191,   0, 159) medium-dark magenta
-	const float4 colorMarBld = float4(0.0,   0.25,  1.0,   1.0); // #0040FF / (  0,  64, 255) medium blue
-	const float4 colorAliens = float4(0.6,   1.0,   0.0,   1.0); // #99FF00 / (153, 255,   0) light green
-	const float4 colorGorgie = float4(0.7,   0.35,  0.0,   1.0); // #B35900 / (179,  89,   0) medium-dark orange
-	const float4 colorAliBld = float4(0.0,   0.15,  0.025, 1.0); // #002606 / (  0,  38,   6) dark green
-	const float4 colorOthers = float4(0.27,  0.0,   0.797, 1.0); // #4500CB / ( 69,   0, 203) medium-dark blue-(nice)-magenta
+	const float4 colorMarine = float4(0.75, 0,    0.625, 1.0);
+	const float4 colorMarBld = float4(0,    0.25, 1,     1.0);
+	const float4 colorGorgie = float4(0.7,  0.35, 0,     1.0);
+	const float4 colorAliens = float4(0.6,  1,    0,     1.0);
+	const float4 colorEntity = float4(0,    0.15, 0.025, 1.0);
+	const float4 colorOthers = float4(0.27, 0,    0.797, 1.0);
 
 	float2 texCoord = input.texCoord;
 	float4 inputPixel = tex2D(baseTexture, texCoord);
+	if (amount == 0)
+	{
+		return inputPixel;
+	}
 
 	float4 depthdata = tex2D(depthTexture, texCoord);
 	float enttype = depthdata.g;
 	float depthC = depthdata.r;
+	float edge =
+		abs(clamp_tex2D(depthTexture, (texCoord + rcpFrame * float2(+1, 0))).r - depthC) +
+		abs(clamp_tex2D(depthTexture, (texCoord + rcpFrame * float2(-1, 0))).r - depthC) +
+		abs(clamp_tex2D(depthTexture, (texCoord + rcpFrame * float2(0, +1))).r - depthC) +
+		abs(clamp_tex2D(depthTexture, (texCoord + rcpFrame * float2(0, -1))).r - depthC);
 
-	// iterate neighboring pixels, and compute a normalized value indicating how edgy and cringe this one is.
-	float edgeStrength =
-		abs(clamp_tex2D(depthTexture, (texCoord + rcpFrame * float2(+1.0, 0.0))).r - depthC) +
-		abs(clamp_tex2D(depthTexture, (texCoord + rcpFrame * float2(-1.0, 0.0))).r - depthC) +
-		abs(clamp_tex2D(depthTexture, (texCoord + rcpFrame * float2(0.0, +1.0))).r - depthC) +
-		abs(clamp_tex2D(depthTexture, (texCoord + rcpFrame * float2(0.0, -1.0))).r - depthC);
-
-	if (enttype > 0.5) // entities
+	if (enttype > 0.99) // marine
 	{
-		if (enttype > 0.99) // marines
-		{
-			return lerp(inputPixel, colorMarine, entColorStrength) + (edgeStrength * colorMarine);
-		}
-		else if (enttype > 0.97) // marine structure
-		{
-			return lerp(inputPixel, colorMarBld, entColorStrength) + (edgeStrength * colorMarBld);
-		}
-		else if (enttype > 0.95) // aliens
-		{
-			return lerp(inputPixel, colorAliens, amount * entColorStrength) + (edgeStrength * colorAliens);
-		}
-		else if (enttype > 0.93) // gorges
-		{
-			return lerp(inputPixel, colorGorgie, amount * entColorStrength) + (edgeStrength * colorGorgie);
-		}
-		else if (enttype > 0.89) // alien structures
-		{
-			return lerp(inputPixel, colorAliBld, amount * entColorStrength) + (edgeStrength * colorAliBld);
-		}
-		else // other scene entities and ready room players
-		{
-			return lerp(inputPixel, colorOthers, amount * entColorStrength * 0.25) + (edgeStrength * colorOthers);
-		}
+		return lerp(inputPixel, colorMarine, entColorStrength) + (edge * colorMarine);
+	}
+	if (enttype > 0.97) // marine structure
+	{
+		return lerp(inputPixel, colorMarBld, entColorStrength) + (edge * colorMarBld);
+	}
+	if (enttype > 0.95) // alien, with transition
+	{
+		return lerp(inputPixel, colorAliens, amount * entColorStrength) + (edge * colorAliens);
+	}
+	if (enttype > 0.93) // gorge, with transition
+	{
+		return lerp(inputPixel, colorGorgie, amount * entColorStrength) + (edge * colorGorgie);
+	}
+	if (enttype > 0.89) // alien structure, with transition
+	{
+		return lerp(inputPixel, colorEntity, amount * entColorStrength) + (edge * colorEntity);
+	}
+	if (enttype > 0.5) // other scene entities and ready room
+	{
+		return lerp(inputPixel, colorOthers, amount * entColorStrength * 0.5) + (edge * colorOthers);
 	}
 
 	// world geometry, viewmodel, etc.
 	float softSkybox = pow(edge * 0.9, 2.3) * step(depthC, 100);
-	float softEdge = min(pow(softSkybox * 0.1, 3), 0.001);
-	float darkmode = lerp(0, relative_luminance(inputPixel.rgb), amount * staticBrightness);
+	float softenEdge = min(pow(softSkybox * 0.1, 3), 0.001);
+	float darkmode = lerp(0, relative_luminance(inputPixel), amount * staticBrightness);
 
-	if (softEdge > 0.000001)
+	if (softenEdge > 0)
 	{
-		float4 colorStaticGeom = float4(float3(darkmode + softEdge), 1.0);
-		return lerp(colorStaticGeom, colorEdging, amount * edgColorStrength) + (edgeStrength * colorEdging);
+		return darkmode + softenEdge;
 	}
-	else
-	{
-		return float4(darkmode);
-	}
+	return darkmode;
 }
